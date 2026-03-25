629f4de6602c2a16a24329b1241070c5c25965be
myselfimprovement-admin <vutevabozhidara@gmail.com>
2026-03-25 22:54:34 +0200

    Added: login component   - styles in css file    - html structure using reactive form    - added logic and make request to api through auth service in ts file Changed: (super-mamas-item component)      - user.username doesn't exist anymore in user interface. Replaced it with user.firstName and user.lastName

--- src/app/features/auth/login/login.component.ts
+++ src/app/features/auth/login/login.component.ts
@@ -1,11 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule , Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthService } from '../../../core/services/auth.service';
+import { InputErrorDirective } from '../../../shared/directives/input-error.directive';
+import { emailValidator } from '../../../shared/validators/email.validator';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule , RouterLink, InputErrorDirective], 
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
 
+export class LoginComponent {
+  private authService = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, emailValidator()]],
+    password: ['', [Validators.required, Validators.minLength(5)]]
+  })
+
+  isLoading = false;
+  errorMessage = '';
+
+  onLogin(): void {
+    if(this.loginForm.invalid){
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    this.isLoading = true;
+    this.errorMessage = '';
+
+    const { email, password } = this.loginForm.value;
+
+    this.authService.login({email, password}).subscribe({
+      next: (user) => {
+        this.authService.setSession(user);
+        this.isLoading = false;
+        this.router.navigate(['/recipes']);
+      },
+      error: (err) => {
+        this.isLoading = false;
+        this.errorMessage = err.error?.message || 'Неуспешно влизане в профила. Опитай отново.';
+      }
+    })
+  }
 }
